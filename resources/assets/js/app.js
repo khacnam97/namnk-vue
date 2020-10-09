511642debe5181baf493ce52d86149ccf1ae135c
Your Name <you@example.com>
2020-10-09 18:08:54 +0700

    aa

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -14,9 +14,19 @@ window.Vue = require('vue');
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('my-component', require('./components/MyComponent.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+const router = new VueRouter({ mode: 'history'});
+const app = new Vue(Vue.util.extend({ router })).$mount('#app');
+
+// const app = new Vue({
+//     el: '#app'
+// });
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+// <?php
+
+// Route::get('/{any}', function () {
+//   return view('post');
+// })->where('any', '.*');
